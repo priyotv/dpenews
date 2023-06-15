04e4f267bf9d213abf03444c1aec0db2fc2af87a
priyotv <136699038+priyotv@users.noreply.github.com>
2023-06-15 07:13:09 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					`https://dpenews.com/en/` + encodeURI(path as string)
 				}`,
 			},
 		};
